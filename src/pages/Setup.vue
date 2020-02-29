15bd27c1494473a3dc5bb62a5559b31a33207bd8
Ben Samworth <bsamworthdev@outlook.co.uk>
2020-02-29 22:28:56 +0000

    Added setup.vue

---
+++ src/pages/Setup.vue
@@ -0,0 +1,31 @@
+<template>
+  <main-layout>
+    <div id="app">
+      <entry-form msg="Participants"/>
+    </div>
+  </main-layout>
+</template>
+
+<script>
+import EntryForm from '../components/EntryForm.vue'
+import MainLayout from '../layouts/Main.vue'
+
+export default {
+  name: 'Setup',
+  components: {
+    EntryForm,
+    MainLayout
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
